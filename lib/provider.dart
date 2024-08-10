31156e68431fa8f14533bf3a87c18f55507cfada
21MH1A0579 <21mh1a0579@acoe.edu.in>
2024-08-10 13:08:10 +0530

    created a function for checking result of token

--- lib/provider.dart
+++ lib/provider.dart
@@ -2,27 +2,49 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+final urlheaders = {
+  "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
+  "x-rapidapi-key":  dotenv.get("API_KEY"),
+  "content-type": "application/json",
+  "accept": "application/json",
+};
+
+Future<void>CompilationResult(String token) async{
+try{
+    final response=await http.get(
+      Uri.parse('https://judge0-ce.p.rapidapi.com/submissions/$token?base64_encoded=true'),
+      headers: urlheaders,
+    );
+    print(response.statusCode.toString()+response.body);
+}
+catch(e){
+
+}
+
+}
+
+
+
+
+
+
+
 //get language id
 int GetLangId(String lang) {
   switch (lang) {
     case 'python':
-      return 71; // Python
+      return 71;
     case 'cpp':
-      return 54; // C++
+      return 54;
     case 'java':
-      return 62; // Java
+      return 62;
     default:
-      return 71; // Default to Python
+      return 71;
   }
 }
 // Code Compilation
 CodeCompile(String code,String language) async{
-  final urlheaders = {
-    "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
-    "x-rapidapi-key":  dotenv.get("API_KEY"),
-    "content-type": "application/json",
-    "accept": "application/json",
-  };
+
   final urlbody = jsonEncode({
     "source_code": code,
     "stdin": "",
@@ -37,6 +59,7 @@ CodeCompile(String code,String language) async{
    if(response.statusCode==201)
      {
         final json=jsonDecode(response.body);
+         await CompilationResult(json['token']);
 
      }
    else{
